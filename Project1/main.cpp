b9b3fa70a6e5c3adc23d54acaa9cf705f6805462
炳男 辛 <1341398182@qq.com>
2019-03-23 00:43:27 +0800

    OJ

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,32 +1,22 @@
 #include <iostream>
 #include <cstdio>
-#include <cstring>
-#include <cmath>
+#include <cctype>
 using namespace std;
 
-bool isPrime(int n) {
-	if (n == 1) return 0;
-	if (n == 2) return 1;
-	if (!(n % 2)) return 0;
-	int m = int(sqrt(n)) + 1;
-	for (int i = 3; i < m; i += 2)
-		if (!(n%i)) return 0; 
-	return 1;
-}
-bool isRevered(int n) {
-	char str[15];
-	int len;
-	sprintf(str, "%d", n);
-	len = strlen(str);
-	for (int i = 0; i < (len + 1) / 2; i++) {
-		if (str[i] != str[len - i -1])return 0;
-	}
-	return 1;
-}
-
 int main() {
-	int n;
-	cin >> n;
-	cout << (isPrime(n) && isRevered(n) ? "Yes" : "No");
+	bool hasLetter = false
+		, hasDigit = false;
+	char c;
+	while (cin >> c) {
+		if (isdigit(c)) hasDigit = true;
+		else if (isalpha(c))hasLetter = true;
+	}
+	if (hasDigit && hasLetter) {
+		cout << "mixed";
+	} else if (hasDigit) {
+		cout << "digit";
+	} else {
+		cout << "letter";
+	}
 	return 0;
 }
