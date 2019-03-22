c1d4ca51f60dbf217ed29eda34b043ee558b52fd
炳男 辛 <1341398182@qq.com>
2019-03-22 23:16:20 +0800

    1054

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -2,8 +2,21 @@
 
 using namespace std;
 
+int a[20] = { 0,1,1,1 };
+
 int main() {
-
-
+	int n;
+	int sum = 0;
+	cin >> n;
+	if (n < 4) {
+		sum = n;
+	} else {
+		sum = 3;
+		for (size_t i = 4; i <= n; i++) {
+			a[i] = a[i - 1] + (a[i - 2] + a[i - 3])*i;
+			sum += a[i];
+		}
+	}
+	cout << sum;
 	return 0;
 }
