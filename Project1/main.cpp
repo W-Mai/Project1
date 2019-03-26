d0ee0d45754bdd9f5c593be4bfcb1cd3ce8bd488
炳男 辛 <1341398182@qq.com>
2019-03-26 23:05:01 +0800

    zyq

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,37 +1,80 @@
-#include <iostream>
+//#include <stdio.h>
+//
+//double F(double x, double y); //声明
+//void F2();
+//
+//int x = 0;
+//char F3(int c, double b, char d);
+//
+//int main() { //入口函数
+//	char str = 'A';
+//	/*	char		1
+//		short		2
+//		int			2<=  <=4  4
+//		long		4
+//		long long	8
+//
+//		float		4
+//		double		8
+//
+//		void		1
+//
+//		数据类型就是如何解释这么长度的数据
+//
+//		|    |     |    |     |     |
+//
+//		强制类型转换
+//
+//		小端存储
+//
+//		FF FF FF FF
+//		255
+//
+//	*/
+//	// double x, y, result;
+//	/*scanf("%lf,%lf", &x, &y);*/
+//	//辛弃疾；
+//	//心气急；
+//
+//	//result = F(x, y);
+//	//printf("%lf", result);
+//
+//	printf("%d\n", x);
+//	F2();
+//	printf("%d\n", x);
+//	
+//	return 0;
+//}
+//
+//double F(double x, double y) { //定义
+//	int a;
+//	a = x * y;
+//
+//	return a + y;
+//}
+//
+//void F2() {} //执行一个动作
+//
+//char F3(int c, double b, char d) {
+//
+//}
 
-#define COMPAREFUNC(a,b,t) (a->t == b->t ? a->ID > b->ID:a->t < b->t)
+#include <stdio.h>
 
-using namespace std;
-
-struct Stu {
-	int ID;
-	int English;
-	int Math;
-	int Total;
-};
-
-int compare1(const Stu * a, const Stu * b) { return  COMPAREFUNC(a, b, English); }
-int compare2(const Stu * a, const Stu * b) { return COMPAREFUNC(a, b, Math); }
-int compare3(const Stu * a, const Stu * b) { return COMPAREFUNC(a, b, Total); }
+int vowel(char ch);
 
 int main() {
-	int n, type;
-	Stu * stus;
-	int(*funcs[3])(const Stu * a, const Stu * b) = { compare1, compare2 , compare3 };
-	cin >> n;
-	stus = new Stu[n];
-	for (int i = 0; i < n; i++) {
-		int M, E;
-		cin >> stus[i].ID >> E >> M;
-		stus[i].English = E;
-		stus[i].Math = M;
-		stus[i].Total = M + E;
+	char ch;
+	int c = 0;
+	while (ch = getchar(), ch != '\n') {
+		if (vowel(ch) == 1) c++;
 	}
-	cin >> type;
-	qsort(stus, n, sizeof(Stu), (int (*)(const void *, const void *))funcs[type - 1]);
-	for (int i = 0; i < n; i++) {
-		cout << stus[i].ID << " " << stus[i].English << " " << stus[i].Math << endl;
-	}
+	printf("%d", c);
 	return 0;
+}
+
+int vowel(char ch) {
+	if (ch == 'a'|| ch == 'i'|| ch =='o'|| ch == 'u' || ch == 'e' || ch == 'A' || ch == 'I' || ch == 'E' || ch == 'O' || ch == 'U')
+		return 1;
+	else  return 0;
 }
